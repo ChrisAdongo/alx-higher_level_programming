debb1289b57e24bc61396e8f3deae2a2dd77442f
Chris Adongo <119563006+ChrisAdongo@users.noreply.github.com>
2024-02-13 10:30:27 +0300

    Add 13-main.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+// Check if there are no arguments
+if (args.length === 0) {
+  console.log(0);
+} else if (args.length === 1) {
+  console.log(0);
+} else {
+  // Convert arguments to integers
+  const integers = args.map(arg => parseInt(arg, 10));
+
+  // Find the second biggest integer
+  const sortedIntegers = integers.sort((a, b) => b - a);
+  console.log(sortedIntegers[1]);
+}
